191f3adf43ba4f201fc8091102c99bfa42054007
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-09-11 10:16:16 +0100

    redaction de la classe Transaction qui represente les operations bancaires et l'historique

--- src/com/nexabank/model/Transaction.java
+++ src/com/nexabank/model/Transaction.java
@@ -1,4 +1,52 @@
 package com.nexabank.model;
-
+import java.time.LocalDate;
 public class Transaction {
+
+    private int idTransaction;
+    private TypeTransaction type;
+    private double montant;
+    private LocalDate date;
+    private Integer compteSource;
+    private Integer compteDestination;
+
+    public Transaction(int idTransaction, TypeTransaction type, double montant,
+                       LocalDate date, Integer compteSource, Integer compteDestination) {
+        this.idTransaction = idTransaction;
+        this.type = type;
+        this.montant = montant;
+        this.date = date;
+        this.compteSource = compteSource;
+        this.compteDestination = compteDestination;
+    }
+    public int getIdTransaction() {
+        return idTransaction;
+    }
+    public TypeTransaction getType() {
+        return type;
+    }
+    public double getMontant() {
+        return montant;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public Integer getCompteSource() {
+        return compteSource;
+    }
+    public Integer getCompteDestination() {
+        return compteDestination;
+    }
+
+
+    public String versLigneReleve() {
+        String source = (compteSource == null) ? "null" : String.valueOf(compteSource);
+        String destination = (compteDestination == null) ? "null" : String.valueOf(compteDestination);
+        return date + " " + type + " " + montant + "€ " + source + " " + destination;
+    }
+
+
+    @Override
+    public String toString() {
+        return versLigneReleve();
+    }
 }
