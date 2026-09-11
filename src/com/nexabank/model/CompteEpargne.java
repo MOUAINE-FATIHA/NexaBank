d4aac8ff37fe228aef16a09914f60b62cd36a865
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-09-11 10:28:07 +0100

    creation de CompteEpargne

--- src/com/nexabank/model/CompteEpargne.java
+++ src/com/nexabank/model/CompteEpargne.java
@@ -1,4 +1,26 @@
 package com.nexabank.model;
+public class CompteEpargne extends Compte {
+    private double tauxInteret;
 
-public class CompteEpargne {
+    public CompteEpargne(int numCompte, double solde) {
+        super(numCompte, solde);
+        this.tauxInteret = 0.02;
+    }
+    public double getTauxInteret() {
+        return tauxInteret;
+    }
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+    public double calculerInteret() {
+        return this.solde * this.tauxInteret;
+    }
+    @Override
+    public String toString() {
+        return "CompteEpargne{" +
+                "numCompte=" + numCompte +
+                ", solde=" + solde +
+                ", tauxInteret=" + tauxInteret +
+                '}';
+    }
 }
