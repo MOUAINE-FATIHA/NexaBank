c0697641acd28445c220dd650159a988b3f87872
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-09-11 10:09:50 +0100

    creation de la classe gestionnaire

--- src/com/nexabank/model/Gestionnaire.java
+++ src/com/nexabank/model/Gestionnaire.java
@@ -1,4 +1,48 @@
 package com.nexabank.model;
+import com.nexabank.exception.ErreurFichierException;
+public class Gestionnaire extends Personne {
 
-public class Gestionnaire{
+    private int idGestionnaire;
+    public Gestionnaire(int idGestionnaire, String nom, String prenom, String email, String password) {
+        super(nom, prenom, email, password);
+        this.idGestionnaire = idGestionnaire;
+    }
+
+    public int getIdGestionnaire() {
+        return idGestionnaire;
+    }
+    public void setIdGestionnaire(int idGestionnaire) {
+        this.idGestionnaire = idGestionnaire;
+    }
+    public Compte creerCompte(Client client, String typeCompte, int numCompte) {
+        Compte nouveauCompte;
+        if (typeCompte.equalsIgnoreCase("Courant")) {
+            nouveauCompte = new CompteCourant(numCompte, 0.0);
+        } else if (typeCompte.equalsIgnoreCase("Epargne")) {
+            nouveauCompte = new CompteEpargne(numCompte, 0.0);
+        } else {
+            throw new IllegalArgumentException("Type de compte inconnu : " + typeCompte);
+        }
+        client.ajouterCompte(nouveauCompte);
+        return nouveauCompte;
+    }
+    public void cloturerCompte(Client client, String numCompte) {
+        if (!client.getComptes().containsKey(numCompte)) {
+            throw new IllegalArgumentException("Compte introuvable.");
+        }
+        client.getComptes().remove(numCompte);
+    }
+    public void mettreAJourClient(Client client, String nom, String prenom, String email) {
+        client.setNom(nom);
+        client.setPrenom(prenom);
+        client.setEmail(email);
+    }
+    @Override
+    public String toString() {
+        return "Gestionnaire{" +
+                "idGestionnaire=" + idGestionnaire +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                '}';
+    }
 }
