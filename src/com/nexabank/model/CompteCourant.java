944aeaf6019e03209ebb021bae5458e3bd8a9a1c
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-09-11 10:26:31 +0100

    creation de CompteCourant

--- src/com/nexabank/model/CompteCourant.java
+++ src/com/nexabank/model/CompteCourant.java
@@ -1,4 +1,24 @@
 package com.nexabank.model;
+public class CompteCourant extends Compte {
 
-public class CompteCourant {
+    private double decouvertAutorise;
+    public CompteCourant(int numCompte, double solde) {
+        super(numCompte, solde);
+        this.decouvertAutorise = 0.0;
+    }
+    public double getDecouvertAutorise() {
+        return decouvertAutorise;
+    }
+    public void autoriserDecouvert(double montant) {
+        this.decouvertAutorise = montant;
+    }
+
+    @Override
+    public String toString() {
+        return "CompteCourant{" +
+                "numCompte=" + numCompte +
+                ", solde=" + solde +
+                ", decouvertAutorise=" + decouvertAutorise +
+                '}';
+    }
 }
