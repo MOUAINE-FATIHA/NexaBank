fac25bc359ed2d957a5a669aaaaeebda472a77f2
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-09-11 10:18:38 +0100

    redaction des exceptions personnalisees

--- src/com/nexabank/exception/ErreurFichierException.java
+++ src/com/nexabank/exception/ErreurFichierException.java
@@ -1,7 +1,7 @@
 package com.nexabank.exception;
 
 public class ErreurFichierException extends RuntimeException {
-  public ErreurFichierException(String message) {
-    super(message);
-  }
+    public ErreurFichierException(String message) {
+        super(message);
+    }
 }
