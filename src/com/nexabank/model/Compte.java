50785e4dbc06e9287b0f21d13d9eb38ec227ea8f
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-09-11 10:25:19 +0100

    creation de la classe abstraite Compte

--- src/com/nexabank/model/Compte.java
+++ src/com/nexabank/model/Compte.java
@@ -1,4 +1,33 @@
 package com.nexabank.model;
+import java.util.HashSet;
 
 public abstract class Compte {
+    protected int numCompte;
+    protected double solde;
+    protected HashSet<Transaction> historique;
+    public Compte(int numCompte, double solde) {
+        this.numCompte = numCompte;
+        this.solde = solde;
+        this.historique = new HashSet<>();
+    }
+    public int getNumCompte() {
+        return numCompte;
+    }
+    public double getSolde() {
+        return solde;
+    }
+    public HashSet<Transaction> getHistorique() {
+        return historique;
+    }
+    private int genererIdTransaction() {
+        return (int) (Math.random() * 1_000_000);
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "numCompte=" + numCompte +
+                ", solde=" + solde +
+                '}';
+    }
 }
